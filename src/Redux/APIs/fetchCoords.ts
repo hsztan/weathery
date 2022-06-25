29dd438dffbfd4f10f188ef222a90160dd0192a7
Henry Nawrocki <hdev@sztanski.com>
2022-06-25 09:44:07 -0500

    Removed heroku-nocors prefix from geocoding endpoint

--- src/Redux/APIs/fetchCoords.ts
+++ src/Redux/APIs/fetchCoords.ts
@@ -3,7 +3,7 @@ import { Coords } from '../../types/types';
 async function fetchCoords(address: string): Promise<Coords> {
   try {
     const response = await fetch(
-      `https://cors-anywhere.herokuapp.com/https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=${address}&benchmark=Public_AR_Current&format=json`
+      `https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=${address}&benchmark=Public_AR_Current&format=json`
     );
     const data = await response.json();
     const coords = data.result.addressMatches[0].coordinates;
