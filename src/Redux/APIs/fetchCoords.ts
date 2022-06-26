8430904054bcb9cc541e8fbbd9fe55c4232e99af
Henry Nawrocki <hdev@sztanski.com>
2022-06-26 17:40:41 -0500

    Latest deploy

--- src/Redux/APIs/fetchCoords.ts
+++ src/Redux/APIs/fetchCoords.ts
@@ -2,8 +2,15 @@ import { Coords } from '../../types/types';
 
 async function fetchCoords(address: string): Promise<Coords> {
   try {
+    const endpoint = `https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=${address}&benchmark=Public_AR_Current&format=json`;
     const response = await fetch(
-      `https://cors-anywhere.herokuapp.com/https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=${address}&benchmark=Public_AR_Current&format=json`
+      `https://cors-anywhere.herokuapp.com/${endpoint}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Origin: 'https://geocoding.geo.census.gov',
+        },
+      }
     );
     const data = await response.json();
     const coords = data.result.addressMatches[0].coordinates;
